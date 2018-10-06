79a3a9af6bc023ff3c002ba12a41ad1cdf060f55
nodejh <jianghangscu@gmail.com>
2018-10-06 16:05:25 +0800

    feat: 递归遍历 tree

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const utils = require('../utils');
 
-const { getSelectedNode } = utils;
+const { getSelectedNode, parseTreeJson } = utils;
 
 const notes = [
   {
@@ -114,12 +114,199 @@ const notes = [
 ];
 
 
+// const notes = {
+//   key: 0,
+//   children: [
+//     {
+//         key: '0-0',
+//         title: 'test11',
+//         topping: true,
+//         children: [],
+//         articles: [
+//             {
+//                 title: '文档标题示例5',
+//                 content:
+//                     'Racing car sprays burning fuel into crowd.Racing car sprays burning fuel into crowd.Racing car sprays burning fuel into crowd.',
+//                 date: '2018-06-28',
+//             },
+//             {
+//                 title: '文档标题示例6',
+//                 content:
+//                     'Racing car sprays burning fuel into crowd.Racing car sprays burning fuel into crowd.',
+//                 date: '2018-06-28',
+//                 topping: true,
+//             },
+//             {
+//                 title: '文档标题示例7',
+//                 content: 'Racing car sprays burning fuel into crowd.',
+//                 date: '2018-06-28',
+//             },
+//             {
+//                 title: '文档标题示例8',
+//                 content: 'Racing car sprays burning fuel into crowd.',
+//                 date: '2018-06-28',
+//             },
+//         ],
+//     },
+//     { key: '0-1', title: 'test12', topping: true, children: [], articles: [] },
+//     {
+//         key: '0-2',
+//         title: 'test13',
+//         children: [
+//             { key: '0-2-0', title: 'test3-1' },
+//             {
+//                 key: '0-2-1',
+//                 title: 'test3-2',
+//                 children: [
+//                     {
+//                         key: '0-2-1-0',
+//                         title: 'test3-3',
+//                     },
+//                 ],
+//             },
+//         ],
+//         articles: [
+//             {
+//                 title: '文档标题示例55',
+//                 content: 'Racing car sprays burning fuel into crowd.',
+//                 date: '2018-06-28',
+//             },
+//             {
+//                 title: '文档标题示例47',
+//                 content: 'Racing car sprays burning fuel into crowd.',
+//                 date: '2018-06-28',
+//                 topping: true,
+//             },
+//         ],
+//     },
+//     {
+//         key: '0-3',
+//         title: 'test4',
+//         articles: [
+//             {
+//                 title: '文档标题示例4',
+//                 content: 'Racing car sprays burning fuel into crowd.',
+//                 date: '2018-06-28',
+//             },
+//         ],
+//     },
+//     {
+//         key: '0-4',
+//         title: 'test5',
+//         articles: [
+//             {
+//                 title: '文档标题示例4',
+//                 content: 'Racing car sprays burning fuel into crowd.',
+//                 date: '2018-06-28',
+//             },
+//         ],
+//     },
+//     {
+//         key: '0-5',
+//         title: 'test6',
+//         articles: [
+//             {
+//                 title: '文档标题示例4',
+//                 content: 'Racing car sprays burning fuel into crowd.',
+//                 date: '2018-06-28',
+//             },
+//         ],
+//     },
+//     {
+//         key: '0-6',
+//         title: 'trash',
+//         trash: true,
+//         articles: [
+//             {
+//                 title: '文档标题示例4',
+//                 content: 'Racing car sprays burning fuel into crowd.',
+//                 date: '2018-06-28',
+//             },
+//         ],
+//     },
+//   ];
+// }
+
+
 describe('getSelectedNode', function() {
+
+  it('获取子节点 0-0', function() {
+    const res = getSelectedNode('0-0', notes);
+    const node =  {
+      key: '0-0',
+      title: 'test11',
+      topping: true,
+      children: [],
+      articles: [
+          {
+              title: '文档标题示例5',
+              content:
+                  'Racing car sprays burning fuel into crowd.Racing car sprays burning fuel into crowd.Racing car sprays burning fuel into crowd.',
+              date: '2018-06-28',
+          },
+          {
+              title: '文档标题示例6',
+              content:
+                  'Racing car sprays burning fuel into crowd.Racing car sprays burning fuel into crowd.',
+              date: '2018-06-28',
+              topping: true,
+          },
+          {
+              title: '文档标题示例7',
+              content: 'Racing car sprays burning fuel into crowd.',
+              date: '2018-06-28',
+          },
+          {
+              title: '文档标题示例8',
+              content: 'Racing car sprays burning fuel into crowd.',
+              date: '2018-06-28',
+          },
+      ],
+    };
+    assert.deepEqual(res, node);
+  })
+
   it('获取子节点 0-1', function() {
     const res = getSelectedNode('0-1', notes);
     const node =  { key: '0-1', title: 'test12', topping: true, children: [], articles: [] };
     assert.deepEqual(res, node);
   })
+
+  it('获取子节点 0-2', function() {
+    const res = getSelectedNode('0-2', notes);
+    const node = {
+      key: '0-2',
+      title: 'test13',
+      children: [
+          { key: '0-2-0', title: 'test3-1' },
+          {
+              key: '0-2-1',
+              title: 'test3-2',
+              children: [
+                  {
+                      key: '0-2-1-0',
+                      title: 'test3-3',
+                  },
+              ],
+          },
+      ],
+      articles: [
+          {
+              title: '文档标题示例55',
+              content: 'Racing car sprays burning fuel into crowd.',
+              date: '2018-06-28',
+          },
+          {
+              title: '文档标题示例47',
+              content: 'Racing car sprays burning fuel into crowd.',
+              date: '2018-06-28',
+              topping: true,
+          },
+      ],
+    };
+    assert.deepEqual(res, node);
+  })
+
 
   it('获取子节点 0-2-0', function() {
     const res = getSelectedNode('0-2-0', notes);
--- utils.js
+++ utils.js
@@ -1,20 +1,29 @@
+var parseTreeJson = function(treeNodes){
+  if (!treeNodes || !treeNodes.length) return;
+
+   for (var i = 0, len = treeNodes.length; i < len; i++) {
+
+        var childs = treeNodes[i].children;
+
+        console.log(treeNodes[i].key);
+
+        if(childs && childs.length > 0){
+             parseTreeJson(childs);
+        }
+   }
+};
 const getSelectedNode = (key, tree) => {
-  // for(let i = 0; i < tree.length; i+=1) {
-  //     if (tree[i].children && tree[i].children.length > 0) {
-  //         getSelectedNode(key, tree[i].children);
-  //     }
-  //     if (tree[i].key === key) return tree[i];
-  // }
-  const selectedNode = tree.find(element => {
-      if (element.children && element.children.length > 0) {
-          return getSelectedNode(key, element.children);
-      }
-      // if (element.key === key) return element;
-      return element.key === key;
-  });
-  return selectedNode;
+  if (!tree || !tree.length) return;
+  for (var i = 0, len = tree.length; i < len; i++) {
+       if (key === tree[i].key) {
+         return tree[i]
+       } else if(tree[i].children && tree[i].children.length > 0){
+         return getSelectedNode(key, tree[i].children);
+       }
+  }
 };
 
 module.exports = {
+  parseTreeJson,
   getSelectedNode,
 }
